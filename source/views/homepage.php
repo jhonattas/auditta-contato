8d47a5e9e91461f49ad9e6bc0c24307ce82136f3
Jhonattas Henrique Goncalves <jhonattas@soucriador.com>
2021-03-15 09:39:24 -0300

    atualiza os relatos

--- source/views/homepage.php
+++ source/views/homepage.php
@@ -140,27 +140,28 @@
   <!-- Testimonials -->
   <section class="testimonials text-center bg-light">
     <div class="container">
-      <h2 class="mb-5">What people are saying...</h2>
+      <h2 class="mb-5">O que alguns clientes dizem de nós...</h2>
       <div class="row">
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>assets/img/testimonials-1.jpg" alt="">
-            <h5>Margaret E.</h5>
-            <p class="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
+            <!-- <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>assets/img/testimonials-1.jpg" alt=""> -->
+            <h5>Marcio Dela Marta.</h5>
+            <p class="font-weight-light mb-0">Atendimento excepcional!<br/>
+            Conheço o dono... É muito caprichoso e dedicado!</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>/assets/img/testimonials-2.jpg" alt="">
-            <h5>Fred S.</h5>
-            <p class="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
+            <!-- <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>/assets/img/testimonials-2.jpg" alt=""> -->
+            <h5>Gabriela Delgado</h5>
+            <p class="font-weight-light mb-0">Excelente atendimento. Muito rápidos e prestativos.</p>
           </div>
         </div>
         <div class="col-lg-4">
           <div class="testimonial-item mx-auto mb-5 mb-lg-0">
-            <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>assets/img/testimonials-3.jpg" alt="">
-            <h5>Sarah W.</h5>
-            <p class="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
+            <!-- <img class="img-fluid rounded-circle mb-3" src="<?php echo URL_BASE; ?>assets/img/testimonials-3.jpg" alt=""> -->
+            <h5>Adriano Baldiotti</h5>
+            <p class="font-weight-light mb-0">Equipe extremamente preparada, Profissionais nota 1000!!!</p>
           </div>
         </div>
       </div>
